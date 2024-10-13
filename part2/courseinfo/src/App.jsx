7ec2217b6cb999f0c8ad81915b6a11bb78f55366
Josh <johanicogo@merchantsfleet.com>
2024-10-13 19:21:11 +0800

    2.1: Course information step 6

--- part2/courseinfo/src/App.jsx
+++ part2/courseinfo/src/App.jsx
@@ -19,12 +19,7 @@ const App = () => {
         name: 'State of a component',
         exercises: 14,
         id: 3
-      },
-      {
-        name: 'asdqwe',
-        exercises: 124,
-        id: 4
-      },
+      }
     ]
   }
 
