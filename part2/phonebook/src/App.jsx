93636e329d66333135e715d86855de3e62a986db
Josh <johanicogo@merchantsfleet.com>
2024-10-14 12:26:38 +0800

    2.12: The Phonebook step 7

--- part2/phonebook/src/App.jsx
+++ part2/phonebook/src/App.jsx
@@ -22,7 +22,12 @@ const App = () => {
     if (!!isExist) {
       alert(`${newName} is already added to phonebook`)
     } else {
-      setPersons(persons.concat({name: newName, number: newNumber}))
+      axios.post('http://localhost:3001/persons', {name: newName, number: newNumber})
+      .then(resp => {
+        setPersons(persons.concat({name: newName, number: newNumber}))
+        setNewName('')
+        setNewNumber('')
+      })
     }
     
   }
