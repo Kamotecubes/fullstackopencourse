932432f5f4b65c0fdc5b7566fed10002fe40e353
Josh <johanicogo@merchantsfleet.com>
2024-10-17 13:38:25 +0800

    3.2: Phonebook backend step 2

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -31,18 +31,23 @@ app.get('/', (request, response) => {
 app.get('/api/persons', (request, response) => {
   response.json(persons)
 })
+
+app.get('/info', (request, response) => {
+    const date = new Date()
+    response.send(`<div><p>Phonebook has info for ${persons.length} people</p><p>${date.toString()}</p></div> `)
+  })
 app.get('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    const note = persons.find(note => note.id === id)
-    if (note) {
-    response.json(note)
+    const person = persons.find(person => person.id === id)
+    if (person) {
+    response.json(person)
     } else {
     response.status(404).end()
     }
 })
 app.delete('/api/persons/:id', (request, response) => {
     const id = request.params.id
-    persons = persons.filter(note => note.id !== id)
+    persons = persons.filter(person => person.id !== id)
   
     response.status(204).end()
 })
