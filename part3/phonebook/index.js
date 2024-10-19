2453c784d7169c414c4973ae164a941bc13a9906
Josh <johanicogo@merchantsfleet.com>
2024-10-19 16:13:42 +0800

    3.9 Phonebook backend step 9

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 const app = express()
 
+
 app.use(morgan('tiny'))
+app.use(express.json())
+
+
+app.use(cors())
 
 let persons = [
     { 
