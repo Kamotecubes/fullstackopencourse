7c568e70d04eee096e62a1b83c81c4134c35389c
Josh <johanicogo@merchantsfleet.com>
2024-10-31 19:11:16 +0800

    3.15: Phonebook database, step 3

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -47,14 +47,10 @@ app.get('/api/persons/:id', (request, response) => {
     })
 })
 app.delete('/api/persons/:id', (request, response) => {
-    const id = request.params.id
-    Person.find({id}).then(result => {
-      if (result) {
-        response.status(204).end()
-      } else {
-      response.status(404).end()
-      }
+    Person.findByIdAndDelete(request.params.id).then(result => {
+      response.status(204).end()
     })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
