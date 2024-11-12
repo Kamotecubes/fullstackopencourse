89b750b2265fa46a037aeb8359347e0c8a152eab
Josh <johanicogo@merchantsfleet.com>
2024-11-12 13:45:06 +0800

    4.2 Blog List, step 2, separate index.js

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,19 +1,7 @@
-const express = require('express')
+const app = require('./app') // the actual Express application
 const config = require('./utils/config')
-const app = express()
-const cors = require('cors')
-const blogsRouter = require('./controllers/blogs')
-const mongoose = require('mongoose')
+const logger = require('./utils/logger')
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
-
-app.use(cors())
-app.use(express.json())
-
-app.use('/api/blogs', blogsRouter)
-
-const PORT = config.PORT
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+app.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
 })
