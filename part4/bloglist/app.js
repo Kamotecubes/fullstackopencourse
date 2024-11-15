a07d1a31fc0ef94a796fdc1ad93d712c2dac792b
Josh <johanicogo@merchantsfleet.com>
2024-11-15 16:00:04 +0800

    4.15: Blog List Expansion, step 3

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -6,6 +6,7 @@ const middleware = require('./utils/middleware')
 const app = express()
 const cors = require('cors')
 const blogsRouter = require('./controllers/blogs')
+const usersRouter = require('./controllers/users')
 const mongoose = require('mongoose')
 
 mongoose.connect(config.MONGODB_URI).then(() => {
@@ -20,6 +21,7 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/blogs', blogsRouter)
+app.use('/api/users', usersRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
