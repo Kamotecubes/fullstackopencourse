88d323ac57467f38fe234e17cd7c526bbc7c66b7
Josh <johanicogo@merchantsfleet.com>
2024-11-15 19:39:56 +0800

    4.20*: Blog List Expansion, step 8

--- part4/bloglist/app.js
+++ part4/bloglist/app.js
@@ -20,6 +20,7 @@ mongoose.connect(config.MONGODB_URI).then(() => {
 app.use(cors())
 app.use(express.json())
 app.use(middleware.requestLogger)
+app.use(middleware.tokenExtractor)
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
@@ -27,6 +28,7 @@ app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
+
 
 
 module.exports = app
