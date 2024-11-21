9dea641cb58c15a4cb4bec2f6ea6a5a112b9cbc5
Josh <johanicogo@merchantsfleet.com>
2024-11-21 19:55:54 +0800

    5.14: Blog List Tests, step 2

--- part5/bloglist-frontend/src/components/Blog.test.jsx
+++ part5/bloglist-frontend/src/components/Blog.test.jsx
@@ -3,12 +3,9 @@ import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 describe('<Blog />', () => {
-
+    let container
 
     beforeEach(() => {
-
-    })
-    test('renders the blog\'s title and author, but does not render its URL or number of likes by default.', async () => {
         const blog = {
             title: "skeletons",
             author: "travis scott",
@@ -23,13 +20,27 @@ describe('<Blog />', () => {
         const mockaddLike = vi.fn()
         const mockdeleteBlog = vi.fn()
         const showDelete = true
-        const {container} = render(<Blog key={blog.id} blog={blog} addLike={mockaddLike} deleteBlog={mockdeleteBlog} showDelete={showDelete}/>)
-        
+        container = render(<Blog key={blog.id} blog={blog} addLike={mockaddLike} deleteBlog={mockdeleteBlog} showDelete={showDelete}/>).container
+
+    })
+    test('renders the blog\'s title and author, but does not render its URL or number of likes by default.', async () => {
+
         const titleDiv = container.querySelector('.title')
         const detailsDiv = container.querySelector('.details')
     
         expect(titleDiv).toHaveTextContent('skeletons travis scott')
         expect(detailsDiv).toHaveStyle('display: none')
+    })
+
+    test('URL and number of likes are shown when the button controlling the shown details has been clicked.', async () => {
+
+        const user = userEvent.setup()
+        const viewButton = screen.getByText('view')
+        await user.click(viewButton)
+
+        const detailsDiv = container.querySelector('.details')
+
+        expect(detailsDiv).not.toHaveStyle('display: none')
     })
 })
 
