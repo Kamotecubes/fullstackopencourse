16206c73696213b53583eea55616829e289c56e7
Josh <johanicogo@merchantsfleet.com>
2024-11-22 20:40:18 +0800

    5.17: Blog List End To End Testing, step 1

---
+++ part5/e2e/tests/blog_app.spec.js
@@ -0,0 +1,13 @@
+const { test, expect, describe, beforeEach } = require('@playwright/test')
+
+describe('Blog app', () => {
+    beforeEach(async ({ page }) => {
+        await page.goto('http://localhost:5173')
+    })
+
+    test('Login form is shown', async ({ page }) => {
+        const locator = await page.getByText('login to application')
+        await expect(locator).toBeVisible()
+    })
+})
+
