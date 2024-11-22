01bd5bc167c9c58837c555b8063e0f83d2d63e95
Josh <johanicogo@merchantsfleet.com>
2024-11-22 21:15:15 +0800

    5.18: Blog List End To End Testing, step 2

--- part5/e2e/tests/blog_app.spec.js
+++ part5/e2e/tests/blog_app.spec.js
@@ -1,13 +1,42 @@
 const { test, expect, describe, beforeEach } = require('@playwright/test')
 
+
 describe('Blog app', () => {
-    beforeEach(async ({ page }) => {
+    beforeEach(async ({ page, request }) => {
+        await request.post('http://localhost:3001/api/testing/reset')
+        await request.post('http://localhost:3001/api/users', {
+            data: {
+                name: 'Joshua Icogo',
+                username: 'josh',
+                password: 'asdqwe'
+            }
+        })
         await page.goto('http://localhost:5173')
     })
-
+  
     test('Login form is shown', async ({ page }) => {
         const locator = await page.getByText('login to application')
         await expect(locator).toBeVisible()
+    })
+  
+    describe('Login', () => {
+      test('succeeds with correct credentials', async ({ page }) => {
+        const textboxes = await page.getByRole('textbox').all()
+        await textboxes[0].fill('josh')
+        await textboxes[1].fill('asdqwe')
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('josh logged in')).toBeVisible()
+      })
+  
+      test('fails with wrong credentials', async ({ page }) => {
+        const textboxes = await page.getByRole('textbox').all()
+        await textboxes[0].fill('josh')
+        await textboxes[1].fill('haha')
+        await page.getByRole('button', { name: 'login' }).click()
+
+        await expect(page.getByText('wrong username or password')).toBeVisible()
+      })
     })
 })
 
